cdaacc55cc5d11a7312d2a6f4de1970680e1317c
Raigor Deng <raigordeng@etekcity.com.cn>
2020-12-02 20:13:37 +0800

    add parameter OPTIONS to method apply_to_nacos to make downloading configurations optional

--- nacos-jmeter/rule.py
+++ nacos-jmeter/rule.py
@@ -60,29 +60,26 @@ class Rule(object):
             }
             self.groups.append(debug_group)
 
-    def _sync_one_namespace(self, nacos: Nacos, namespace: str, dst_dir: str) -> list:
+    def _extract_one_namespace(self, nacos: Nacos, namespace: str, **options) -> list:
         """
-        Download configurations of specific namespace.
+        Extract and return configurations existed from one Nacos namespace.
 
-        Hierarchy of downloaded files would be as dst_dir/group/dataId.
-        The destination directory name should be set to namespace name.
+        1. Each element of the returned list is a tuple of (namespace_id, group, data_id).
+        2. Parameter options adds functionalities like downloading configurations.
+           Hierarchy of downloaded files would be dst_dir/namespace/group/dataId.
 
         :param nacos: instance of class Nacos
         :param namespace: name of namespace
-        :param dst_dir: Directory to save configuration files
-        :return: a list of (namespace_id, group, data_id) existed in Nacos
+        :param options: denotes additional functionalities like download configuration files
+        :return: list
         """
         data_id_paths = []
         namespace_id = nacos.namespaces[namespace]["id"]
         for group in self.groups:
             group_name = group["group"]
             data_ids = group["data-ids"]
-            group_dir = f"{dst_dir}/{group_name}"
-            logger.info(f"group: {namespace}:{group_name} (data-ids: {data_ids}, path: {group_dir})")
-            Path(group_dir).mkdir(parents=False, exist_ok=True)
+            logger.info(f"{namespace}:{group_name}:data-ids - {data_ids}")
             for data_id in data_ids:
-                config_file = f"{group_dir}/{data_id}"
-                logger.info(f"{namespace}:{group_name}:{data_id} file path: {config_file}")
                 payload = {
                     "tenant": namespace_id,
                     "group": group_name,
@@ -90,31 +87,40 @@ class Rule(object):
                 }
                 response = requests.get(nacos.get_config_url, params=payload)
 
-                # if configuration exists, download it and save path to list
+                # if configuration exists, download it and save data id path to list
                 if response.status_code == 200:
-                    content = response.text
-                    logger.info(f"{namespace}:{group_name}:{data_id}'s content : {content}")
-                    with open(config_file, "w") as f:
-                        f.write(content)
                     data_id_paths.append(tuple(payload.values()))
+
+                    # if the *options* dict contains key 'dst_dir', the existed configurations would be downloaded.
+                    if "dst_dir" in options.keys():
+                        dst_dir = options["dst_dir"]
+                        group_dir = f"{dst_dir}/${namespace}/{group_name}"
+                        config_file = f"{group_dir}/{data_id}"
+                        Path(group_dir).mkdir(parents=False, exist_ok=True)
+                        logger.info(f"{namespace}:{group_name}:{data_id} - {config_file}")
+                        content = response.text
+                        logger.info(f"{namespace}:{group_name}:{data_id} - \n{content}")
+                        with open(config_file, "w") as f:
+                            f.write(content)
 
             return data_id_paths
 
-    def apply_to_nacos(self, nacos: Nacos, dst_dir) -> list:
+    def apply_to_nacos(self, nacos: Nacos, **options) -> list:
         """
-        Download configuration files from Nacos based on rule.
+        Extract and return configurations existed in Nacos.
+
+        1. Each element of the returned list is a tuple of (namespace_id, group, data_id).
+        2. Parameter options adds functionalities like downloading configurations.
 
         :param nacos: instance of class Nacos
-        :param dst_dir: directory to store downloaded configuration files
-        :return: a list of (namespace_id, group, data_id) existed in Nacos
+        :param options: additional functionalities like downloading configurations
+        :return: list
         """
         data_id_paths = []
         for namespace in self.namespaces:
-            namespace_dir = f"{dst_dir}/{namespace}"
-            logger.info(f"Begin to sync namespace: {namespace} (path: {namespace_dir})")
-            Path(namespace_dir).mkdir(parents=False, exist_ok=True)
-            data_id_paths += self._sync_one_namespace(nacos, namespace, namespace_dir)
-            logger.info(f"End to sync namespace: {namespace}")
+            logger.info(f"Begin to check namespace: {namespace}")
+            data_id_paths += self._extract_one_namespace(nacos, namespace, **options)
+            logger.info(f"End to check namespace: {namespace}")
 
         return data_id_paths
 
