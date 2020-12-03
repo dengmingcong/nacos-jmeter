562147ee9f7b55b15d2592ca4aa4dcbe400ac7fb
dengmingcong <mc.deng@outlook.com>
2020-12-03 23:12:40 +0800

    add new settings for configurations in namespace 'public'

--- bin/make_snapshot.py
+++ bin/make_snapshot.py
@@ -1,4 +1,5 @@
 from loguru import logger
+from pathlib import Path
 import requests
 import yaml
 
@@ -9,7 +10,9 @@ from rule import Rule
 
 def make_snapshot(nacos: Nacos, dst_dir):
     """
-    Make snapshot of Nacos based on specific rules.
+    Make snapshot of Nacos based on the global rule.
+
+    Configurations in namespace 'public' will be downloaded yet.
     """
     assert nacos.is_website_online(), f"Error. Cannot open login page {nacos.login_url} now."
 
@@ -19,12 +22,24 @@ def make_snapshot(nacos: Nacos, dst_dir):
         "dataId": settings.SNAPSHOT_RULE_DATA_ID
     }
     rules_yaml = requests.get(nacos.get_config_url, params=payload).text
-    logger.info(f"YAML content: \n{rules_yaml}")
+    logger.info(f"snapshot rule content: \n{rules_yaml}")
     rules_dict = yaml.safe_load(rules_yaml)
 
     # make snapshot
     rule = Rule(rules_dict["namespaces"], rules_dict["devices"], True)
     rule.apply_to_nacos(nacos, dst_dir=dst_dir)
+
+    # download configurations in namespace 'public'
+    jenkins_jmx_relationship_group_dir = f"{dst_dir}/public/{settings.JENKINS_JMX_RELATIONSHIP_GROUP}"
+    Path(jenkins_jmx_relationship_group_dir).mkdir(parents=True, exist_ok=True)
+    payload_jenkins_jmx_conf = {
+        "group": settings.JENKINS_JMX_RELATIONSHIP_GROUP,
+        "dataId": settings.JENKINS_JMX_RELATIONSHIP_DATA_ID
+    }
+    jenkins_jmx_conf_yaml = requests.get(nacos.get_config_url, params=payload_jenkins_jmx_conf).text
+    logger.info(f"jenkins jmx conf content: \n{jenkins_jmx_conf_yaml}")
+    with open(f"{jenkins_jmx_relationship_group_dir}/{settings.JENKINS_JMX_RELATIONSHIP_DATA_ID}", "w") as f:
+        f.write(jenkins_jmx_conf_yaml)
 
 
 if __name__ == "__main__":
