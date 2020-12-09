bc9ff1c8b9995900375bd6c623867f56596af059
Raigor Deng <raigordeng@etekcity.com.cn>
2020-12-09 15:58:12 +0800

    import module common and refer method defined in it corresponding to class Builder's change

--- bin/init_jenkins_build.py
+++ bin/init_jenkins_build.py
@@ -3,6 +3,7 @@ sys.path.append("../nacos-jmeter")
 
 from builder import Builder
 from testplan import TestPlan
+import common
 
 jenkins_job_name = sys.argv[1]
 jenkins_job_workspace = sys.argv[2]
@@ -12,7 +13,7 @@ test_plan_base_dir = sys.argv[5]
 new_build_xml = sys.argv[6]
 
 build = Builder(jenkins_job_name, jenkins_job_workspace, jmeter_home, test_name, test_plan_base_dir)
-build.concatenate_property_files(f"../snapshot/{jenkins_job_name}.properties", True)
+common.concatenate_files(build.additional_properties, f"../snapshot/{jenkins_job_name}.properties", True)
 test_plan = TestPlan(build.test_plan_abs_path)
 test_plan.change_controller_type(build.test_plan_abs_path)
 build.generate_new_build_xml(new_build_xml)
