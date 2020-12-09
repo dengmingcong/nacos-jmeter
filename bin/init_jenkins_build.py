73802506817cd081a631835e7e65501bbe8c6818
Raigor Deng <raigordeng@etekcity.com.cn>
2020-12-09 08:57:43 +0800

    enable concatenating property files and print the result by default

--- bin/init_jenkins_build.py
+++ bin/init_jenkins_build.py
@@ -12,8 +12,7 @@ test_plan_base_dir = sys.argv[5]
 new_build_xml = sys.argv[6]
 
 build = Builder(jenkins_job_name, jenkins_job_workspace, jmeter_home, test_name, test_plan_base_dir)
+build.concatenate_property_files(f"../snapshot/{jenkins_job_name}.properties", True)
 test_plan = TestPlan(build.test_plan_abs_path)
-print("Converting simple controller to transaction controller...")
 test_plan.change_controller_type(build.test_plan_abs_path)
-print("Generating new build.xml for ant-jmeter...")
 build.generate_new_build_xml(new_build_xml)
