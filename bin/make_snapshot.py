3e98fae026f1f134dd2e4876c9163ad6beef706f
Raigor Deng <raigordeng@etekcity.com.cn>
2020-12-09 17:50:23 +0800

    use different host for different environment

--- bin/make_snapshot.py
+++ bin/make_snapshot.py
@@ -5,6 +5,15 @@ from nacos import Nacos
 import settings
 
 
-snapshot_dir = sys.argv[1]
-nacos_new = Nacos(settings.HOST, settings.PORT)
+stage = sys.argv[1]
+snapshot_dir = sys.argv[2]
+if stage == "ci":
+    host = settings.HOST_CI
+elif stage == "testonline":
+    host = settings.HOST_TESTONLINE
+elif stage == "predeploy":
+    host = settings.HOST_PREDEPLOY
+else:
+    raise ValueError("stage error")
+nacos_new = Nacos(host, settings.PORT)
 nacos_new.make_snapshot(snapshot_dir)
