76aeb57564db557d85b1c93af324965f7b22f3b9
Raigor Deng <raigordeng@etekcity.com.cn>
2020-12-10 13:40:56 +0800

    change corresponding to class Builder

--- bin/init_jenkins_build.py
+++ bin/init_jenkins_build.py
@@ -12,8 +12,10 @@ test_name = sys.argv[4]
 test_plan_base_dir = sys.argv[5]
 new_build_xml = sys.argv[6]
 
-build = Builder(jenkins_job_name, jenkins_job_workspace, jmeter_home, test_name, test_plan_base_dir)
-common.concatenate_files(build.additional_properties, f"../snapshot/{jenkins_job_name}.properties", True)
-test_plan = TestPlan(build.test_plan_abs_path)
-test_plan.change_controller_type(build.test_plan_abs_path)
-build.generate_new_build_xml(new_build_xml)
+build = Builder(jenkins_job_name)
+additional_properties = build.collect_property_files()
+common.concatenate_files(additional_properties, f"../snapshot/{jenkins_job_name}.properties", True)
+test_plan_abs_path = build.test_plan_abs(test_plan_base_dir)
+test_plan = TestPlan(test_plan_abs_path)
+test_plan.change_controller_type(test_plan_abs_path)
+build.generate_new_build_xml(jenkins_job_workspace, jmeter_home, test_name, test_plan_base_dir, new_build_xml)
