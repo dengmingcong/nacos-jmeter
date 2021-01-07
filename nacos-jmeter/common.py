b31b772b17c29b379598be18533823debccab4f9
Raigor Deng <raigordeng@etekcity.com.cn>
2021-01-07 14:19:22 +0800

    read link of javac command if env JAVA_HOME not set

--- nacos-jmeter/common.py
+++ nacos-jmeter/common.py
@@ -40,9 +40,16 @@ def convert_property_file(property_file, out):
                          r"\path\to\jdk\bin\native2ascii.exe -encoding UTF-8 src.properties dst.properties")
     else:
         native2ascii_basename = "native2ascii"
+    # first, get JAVA_HOME from env
     java_home = os.getenv('JAVA_HOME')
-    logger.info(f"JAVA_HOME: {java_home}")
+    # if JAVA_HOME not set, get from javac command
+    if not java_home:
+        logger.debug("env JAVA_HOME not set, read link of javac")
+        java_home = subprocess.check_output(
+            'dirname $(dirname $(readlink -f $(which javac)))', shell=True, universal_newlines=True
+        ).split('\n')[0]
+    logger.debug(f"JAVA_HOME: {java_home}")
     native2ascii_full_path = os.path.join(java_home, 'bin', native2ascii_basename)
-    logger.info(f"native2ascii: {native2ascii_full_path}")
+    logger.debug(f"native2ascii: {native2ascii_full_path}")
     result = subprocess.run([native2ascii_full_path, '-encoding', 'UTF-8', property_file, out])
-    logger.info(f"Arguments: {result.args}")
+    logger.debug(f"Arguments: {result.args}")
