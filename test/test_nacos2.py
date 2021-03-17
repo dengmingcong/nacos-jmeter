a129cefd1a7bd4b536fd1c4139838a1a99ff4cab
Raigor Deng <raigordeng@etekcity.com.cn>
2021-03-17 17:10:55 +0800

    test: test callback which will run multi processes.

--- test/test_nacos2.py
+++ test/test_nacos2.py
@@ -1,3 +1,5 @@
+from multiprocessing import Pool
+import os, time, random
 import sys
 sys.path.append("../nacos-jmeter")
 import time
@@ -9,22 +11,37 @@ SERVER_ADDRESSES = f"{settings.HOST_CI}"
 NAMESPACE = "env-01"
 
 # no auth mode
-client = nacos.NacosClient(SERVER_ADDRESSES, namespace=NAMESPACE)
+client = nacos.NacosClient(SERVER_ADDRESSES)
 # auth mode
 #client = nacos.NacosClient(SERVER_ADDRESSES, namespace=NAMESPACE, username="nacos", password="nacos")
 
 # get config
-data_id = "WiFiBTOnboardingNotify_AirPurifier_LAP-C401S-WUSR_US"
-group = "DEVICE"
+data_id = "nacos.commit.message"
+group = "DEFAULT_GROUP"
 
 
 def println(params):
     print(f"{params['data_id']}hello, properties changes detected.")
 
+def long_time_task(name):
+    print(f"Run task {name} ({os.getpid()})")
+    start = time.time()
+    time.sleep(random.random() * 7)
+    end = time.time()
+    print(f"Task {name} runs {end - start} seconds.")
+
+def test_multi(params):
+    print(f"Parent process {os.getpid()}.")
+    p = Pool()
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print("Waiting for all subprocesses done...")
+    p.close()
+    p.join()
+    print('All subprocesses done.')
 
 if __name__ == "__main__":
     client.set_debugging()
     # client.get_config(data_id, group)
-    for i in range(10):
-        client.add_config_watcher(data_id, group, println)
+    client.add_config_watcher(data_id, group, test_multi)
     time.sleep(10000)
