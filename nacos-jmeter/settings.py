5a2e32c18814f6c87f722a63671b307f1a158345
dengmingcong <mc.deng@outlook.com>
2021-03-21 23:20:43 +0800

    refactor: add configs for collecting properties for stages

--- nacos-jmeter/settings.py
+++ nacos-jmeter/settings.py
@@ -9,6 +9,18 @@ HOST_PREDEPLOY = "10.40.9.73"
 HOST_SIT = "10.40.9.73"
 PORT = 8848
 
+CROSS_ENV_NAMESPACE_ID = "cross-env"
+# denote Jenkins job stage flag and the related namespace id.
+STAGE_NAMESPACES = {
+    "ci": "env-01",
+    "testonline": "env-02",
+    "predeploy": "env-03",
+    "production": "env-04"
+}
+
+# item that comes later in list has higher priority
+TARGET_GROUPS = ["SHARED", "DEVICE"]
+
 SNAPSHOT_RULE_DATA_ID = "nacos.snapshot.rule"
 SNAPSHOT_RULE_GROUP = "DEFAULT_GROUP"
 SYNC_TRIGGER_DATA_ID = "nacos.commit.message"
@@ -22,6 +34,6 @@ ON_SAMPLE_ERROR_ACTION = "stopthread"
 DATA_BASE = os.path.join(PROJECT_ROOT, "data")
 COMMIT_HISTORY = os.path.join(DATA_BASE, "commit.log")
 NACOS_SNAPSHOT_REPO_NAME = "nacos-snapshot"
-NACOS_SNAPSHOT_REPO_DIR = f"d:/03 mp products/01 cg Git代码/{NACOS_SNAPSHOT_REPO_NAME}"
-NACOS_SNAPSHOT_REPO_URL = "git@fangcun.vesync.com:testTeam/nacos-snapshot.git"
+NACOS_SNAPSHOT_REPO_DIR = f"/Users/dengmingcong/SynologyDrive/Workspace/{NACOS_SNAPSHOT_REPO_NAME}"
+NACOS_SNAPSHOT_REPO_URL = "git@github.com:kakaaaluote/nacos-snapshot.git"
 NACOS_CLIENT_DEBUGGING = False
