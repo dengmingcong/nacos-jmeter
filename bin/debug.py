cd9d71415245eafe52c038caffe08f4f362e42bc
Raigor Deng <raigordeng@etekcity.com.cn>
2021-03-23 00:02:03 +0800

    fix: set shell to True when run jmeter to fix  the problem that jmeter cannot be called.

--- bin/debug.py
+++ bin/debug.py
@@ -9,21 +9,21 @@ import settings
 
 
 if __name__ == "__main__":
-    # one of "ci", "testonline", "predeploy", "production"
+    # choose one from "ci", "testonline", "predeploy", "production"
     stage = "ci"
 
-    # get summary configs for specific stage
     snapshot_base = os.path.join(settings.JMETER_HOME, "bin")
+
+    # get summary configs for specific stage
     nacos_client = nacos.NacosClient(settings.HOST_CI, namespace=settings.SUMMARY_NAMESPACE_ID)
     nacos_client.set_options(snapshot_base=snapshot_base)
     nacos_client.get_config(stage, settings.SUMMARY_GROUP)
 
     # rename summary config file
-    summary_file_name = "+".join([stage, settings.SUMMARY_GROUP, settings.SUMMARY_NAMESPACE_ID])
     os.chdir(snapshot_base)
-    old_file = summary_file_name
+    old_file = "+".join([stage, settings.SUMMARY_GROUP, settings.SUMMARY_NAMESPACE_ID])
     new_file = f"{stage}.properties"
     os.rename(old_file, new_file)
 
     # start JMeter with additional properties
-    subprocess.run(["jmeter", "-q", new_file])
+    subprocess.run(f"jmeter -q {new_file}", shell=True)
