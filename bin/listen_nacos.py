3d5ebae3e8663ece549a749506a52589af133e78
Raigor Deng <raigordeng@etekcity.com.cn>
2021-03-23 10:45:23 +0800

    refactor: change Nacos server host for running on Linux later

--- bin/listen_nacos.py
+++ bin/listen_nacos.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     log_dir = settings.LOG_DIR
     Path(log_dir).mkdir(parents=True, exist_ok=True)
     logger.add(f"{log_dir}/" + "syncer_{time}.log", rotation="5 MB", compression="zip", encoding="utf-8")
-    nacos_server = nacosserver.NacosServer(settings.HOST_CI, settings.PORT)
+    nacos_server = nacosserver.NacosServer(settings.HOST_TESTONLINE, settings.PORT)
     s = syncer.NacosSyncer(nacos_server)
     s.run()
