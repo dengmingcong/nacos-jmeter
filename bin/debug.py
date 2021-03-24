b45bd464c4d69c64b8108de41fbb56b464d77533
Raigor Deng <raigordeng@etekcity.com.cn>
2021-03-24 10:18:51 +0800

    fix: delete file if exist to prevent error when renames

--- bin/debug.py
+++ bin/debug.py
@@ -4,6 +4,7 @@ import sys
 root = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(f"{root}/nacos-jmeter")
 
+from loguru import logger
 import nacos
 
 import settings
@@ -14,6 +15,7 @@ if __name__ == "__main__":
     stage = "ci"
 
     snapshot_base = os.path.join(settings.JMETER_HOME, "bin")
+    logger.info(f"snapshot base is set to {snapshot_base}")
 
     # get summary configs for specific stage
     nacos_client = nacos.NacosClient(settings.HOST_CI, namespace=settings.SUMMARY_NAMESPACE_ID)
@@ -24,7 +26,11 @@ if __name__ == "__main__":
     os.chdir(snapshot_base)
     old_file = "+".join([stage, settings.SUMMARY_GROUP, settings.SUMMARY_NAMESPACE_ID])
     new_file = f"{stage}.properties"
+    # delete if already exist
+    if os.path.isfile(new_file):
+        os.remove(new_file)
     os.rename(old_file, new_file)
 
     # start JMeter with additional properties
+    logger.info(f"start JMeter with property file {snapshot_base}/{new_file} loaded")
     subprocess.run(f"jmeter -q {new_file}", shell=True)
