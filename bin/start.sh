46942e3fed4453c945a2fa848dd11fda0ba12cac
Raigor Deng <raigordeng@etekcity.com.cn>
2021-03-24 10:07:03 +0800

    refactor: add scripts to start/stop listening progress

---
+++ bin/start.sh
@@ -0,0 +1,11 @@
+echo > /var/log/nacos-syncer/start.log
+while
+    ! systemctl is-active --quiet nacos.service
+do
+    echo "Service Nacos is not active now, wait for a moment" >> /var/log/nacos-syncer/start.log
+    sleep 10
+done
+
+echo "Service Nacos is active now, begin to listen" >> /var/log/nacos-syncer/start.log
+sleep 30
+nohup /usr/local/bin/python3 /srv/nacos-jmeter/bin/listen_nacos.py > /var/log/nacos-syncer/start.log 2>&1 &
