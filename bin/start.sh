8d067ae1cec90c417c42c36942e7eb61e3127d68
Raigor Deng <raigordeng@etekcity.com.cn>
2021-03-25 09:02:51 +0800

    refactor: do not truncate start.log when start program.

--- bin/start.sh
+++ bin/start.sh
@@ -8,4 +8,4 @@ done
 
 echo "Service Nacos is active now, begin to listen" >> /var/log/nacos-syncer/start.log
 sleep 30
-nohup /usr/local/bin/python3 /srv/nacos-jmeter/bin/listen_nacos.py > /var/log/nacos-syncer/start.log 2>&1 &
+nohup /usr/local/bin/python3 /srv/nacos-jmeter/bin/listen_nacos.py >> /var/log/nacos-syncer/start.log 2>&1 &
