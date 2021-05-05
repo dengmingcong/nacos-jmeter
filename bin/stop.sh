d6a237d52b8ce8cab407464363132930e646c7f3
dengmingcong <mc.deng@outlook.com>
2021-05-05 23:18:05 +0800

    refactor: rename files to differentiate from database listener

--- bin/stop.sh
+++
@@ -1,1 +0,0 @@
-ps -ef | grep '/srv/nacos-jmeter/bin/listen_nacos.py' | grep -v grep | awk '{print $2}' | xargs -r kill -9
---
+++ bin/stop_listen_on_nacos.sh
@@ -0,0 +1,1 @@
+ps -ef | grep '/srv/nacos-jmeter/bin/listen_on_nacos.py' | grep -v grep | awk '{print $2}' | xargs -r kill -9
