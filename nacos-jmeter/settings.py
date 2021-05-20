1e37f601bb1f0b53c172383bd7ed29eb39774cc0
Raigor Deng <raigordeng@etekcity.com.cn>
2021-05-20 14:26:11 +0800

    ci: update jmeter home path to 5.4.1

--- nacos-jmeter/settings.py
+++ nacos-jmeter/settings.py
@@ -10,7 +10,7 @@ NACOS_SNAPSHOT_REPO_DIR = f"/data/qa/{NACOS_SNAPSHOT_REPO_NAME}"
 NACOS_SNAPSHOT_REPO_URL = "git@fangcun.vesync.com:testTeam/nacos-snapshot.git"
 NACOS_CLIENT_DEBUGGING = False
 
-JMETER_HOME = "d:/Program Files (x86)/apache-jmeter-5.4"
+JMETER_HOME = "d:/Program Files (x86)/apache-jmeter-5.4.1"
 
 # Nacos server info
 NACOS_SERVER_HOST_CI = "34.234.176.173"
